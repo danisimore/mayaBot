eb72dacb862b8a5fe43cad4a49fe0c780136292c
danisimore <danisimore@yandex.ru>
2024-01-09 15:32:06 +0300

    созданы этапы сбора данных для регистрации в дисконтной программе

--- states/discount_registration.py
+++ states/discount_registration.py
@@ -10,3 +10,5 @@ class DiscountRegistration(StatesGroup):
     entering_name = State()
     entering_last_name = State()
     entering_patronymic = State()
+    entering_phone_number = State()
+    checking_entered_data = State()
